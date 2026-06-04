4043cee0ee1e8f98d658d601b724cbafa76a555c
Moody Amberdal <129885358+moodyambr@users.noreply.github.com>
2026-06-04 06:55:01 +0200

    Fix: tvinga Celsius med &m i URL

--- vader.sh
+++ vader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATUM=$(date "+%Y-%m-%d %H:%M")
-DATA=$(curl -s "https://wttr.in/Stockholm?format=%t+%C")
+DATA=$(curl -s "https://wttr.in/Stockholm?format=%t+%C&m")
 
 echo "$DATUM — $DATA" >> log.txt
 echo "Loggade: $DATA"
